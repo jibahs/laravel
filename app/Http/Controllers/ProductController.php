b4c742c317f815ad94436db24e488b0518cb7580
jibahs <najibahaisyahibah@gmail.com>
2024-09-03 21:13:55 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::paginate(12);
+
+        return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+            $request->validate([
+            'nama' => 'required',
+            'harga' => 'required|numeric',
+            'foto' => 'required|image|mimes:jpeg,png,jpg'
+            ]);
+
+        $foto = $request->file('foto');
+        $foto->storeAs('public', $foto->hashName());
+
+        Product::create([
+            'nama' => $request->nama,
+            'harga' =>str_replace(".", "", $request->harga),
+            'deskripsi' => $request->deskripsi,
+            'foto' => $foto->hashName()
+        ]);
+
+        return redirect()->route('products.index')->with('success', 'Action successfully completed');
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'harga' => 'required|numeric',
+        ]);
+
+        $product->nama = $request->nama;
+        $product->harga = str_replace(".", "", $request->harga);
+        $product->deskripsi = $request->deskripsi;
+
+        if ($request->file('foto')) {
+
+            Storage::disk('local')->delete('public/' . $product->foto);
+            $foto = $request->file('foto');
+            $foto->storeAs('public', $foto->hashName());
+            $product->foto = $foto->hashName();
+        }
+
+        $product->update();
+
+        return redirect()->route('products.index')->with('success', 'Action successfully completed');
+    }
+    public function destroy(Product $product)
+        {
+            if ($product->foto !== "noimage.png") {
+                Storage::disk('local')->delete('public/' . $product->foto);
+            }
+
+            $product->delete();
+
+            return redirect()->route('products.index')->with('success', 'Action successfully completed');
+        }
+}
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="mt-3">
+<footer style="background-color: #52369f; color: #FFFFFF; padding: 17px; text-align: center;">
+    <p>© 2024 Elora. All rights reserved.</p>
+</footer>
+</div>
